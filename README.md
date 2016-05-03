4ae99ef21503b07f5e342c6511646161b827b05e
Marx Jiao <baidu@baidudeMacBook-Pro.local>
2016-05-03 16:56:39 +0800

    add stack

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavascriptDataStructureAndAlgorithm
 Javascript data structure and algorithm study.
+Javascript数据结构和算法学习
---
+++ Stack/Stack.js
@@ -0,0 +1,68 @@
+/**
+ * @file js实现栈
+ * @author Marx
+ */
+function Stack() {
+    
+    /**
+     * 用数组来保存栈的元素
+     */
+    var items = [];
+    
+    /**
+     * 向栈中添加数据的方法push()
+     * 
+     * @param element 要向栈中添加的数据
+     */
+    this.push = function(element){
+        items.push(element);
+    };
+    
+    /**
+     * 从栈中移除数据的方法pop()
+     */
+    this.pop = function(){
+        return items.pop();
+    };
+    
+    /**
+     * 返回栈顶的元素peek()
+     * 
+     * @return items[items.length-1] 栈顶的元素
+     */
+    this.peek = function(){
+        return items[items.length-1];
+    };
+    
+    /**
+     * 返回是否为空栈
+     * 
+     * @return {boolean} items.length == 0 是否为空栈
+     */
+    this.isEmpty = function(){
+        return items.length == 0;
+    };
+
+    /**
+     * 返回栈的大小
+     * 
+     * @return {number} items.length 栈的大小
+     */
+    this.size = function(){
+        return items.length;
+    };
+
+    /**
+     * 清空栈
+     */
+    this.clear = function(){
+        items = [];
+    };
+    
+    /**
+     * 打印栈
+     */
+    this.print = function(){
+        console.log(items.toString());
+    };
+}
